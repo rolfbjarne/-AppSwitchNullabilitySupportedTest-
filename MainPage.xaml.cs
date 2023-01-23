20c82e05412e42fc78eb17256d10b32b3832ecd0
Rolf Bjarne Kvinge <rolf@xamarin.com>
2023-01-23 22:02:09 +0100

    Initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace AppSwitchNullabilitySupportedTest;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+        var @switch = "System.Reflection.NullabilityInfoContext.IsSupported";
+        if (!AppContext.TryGetSwitch(@switch, out var isEnabled))
+        {
+            CounterBtn.Text = $"Switch is not set";
+        }
+        else
+        {
+            CounterBtn.Text = $"Switch is set to: {isEnabled}";
+        }
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace AppSwitchNullabilitySupportedTest;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
